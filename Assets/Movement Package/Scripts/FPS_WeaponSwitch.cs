4136e70dc324a7f632360ea870d636aed3a97a2a
Az Foxxo <KnotGameStudio@gmail.com>
2022-11-08 00:29:23 +0000

    Improved weapon switch script so mouse up can be used now

--- Assets/Movement Package/Scripts/FPS_WeaponSwitch.cs
+++ Assets/Movement Package/Scripts/FPS_WeaponSwitch.cs
@@ -1,13 +1,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-// https://github.com/AzFoxxo (2022 MIT)
+/*
+    * This script is used to switch between weapons.
+    * It is attached to the player.
+    * To add a new weapon, add it to the weapons list.
+    * To change the weapon, use the scroll wheel or the number keys.
+    * Code by Az Foxxo (https://github.com/AzFoxxo/Scripts)
+    * Anarchist License, MIT Licence, GNU GPL v3.0 Licence, or any later version.
+*/
 
 public class FPS_WeaponSwitch : MonoBehaviour
 {
-    // Weapons (new() will not work due to outdated C# version)
+    // List of weapons
     [SerializeField] List<GameObject> weapons = new List<GameObject>();
+
+    // Current weapon index
     byte currentWeaponIndex;
+
+    // Delay between weapon switch
+    [SerializeField] float switchDelay = 0.25f;
+
+    // Timer for weapon switch delay
+    float switchTimer = 0;
 
     // Activate the default weapon
     private void Awake() {
@@ -19,25 +34,50 @@ public class FPS_WeaponSwitch : MonoBehaviour
     // Switch weapons when letter e is held
     private void Update()
     {
-        // Check for key down
-        if (Input.GetKeyDown(KeyCode.F)) SwitchWeapon();
-        
+        // Switch weapons when mouse wheel is scrolled up
+        if (Input.GetAxis("Mouse ScrollWheel") > 0f) SwitchWeapon();
+
+        // Decrease switch timer
+        switchTimer -= Time.deltaTime;
+
+        // Clamp switch timer
+        if (switchTimer < 0) switchTimer = 0;
     }
 
     // Switch the weapon
     private void SwitchWeapon()
     {
+        // If the switch delay timer is not finished, return
+        if (switchTimer > 0) return;
+
+        // Set the switch timer
+        switchTimer = switchDelay;
+
         // Deactivate the current weapon
         weapons[currentWeaponIndex].SetActive(false);
 
-        // Activate the next weapon
-        currentWeaponIndex = ((byte)(currentWeaponIndex+1 != weapons.Count ? currentWeaponIndex+1 : 0));
-        weapons[currentWeaponIndex].SetActive(true);
+        // Switch to the next weapon
+        currentWeaponIndex++;
 
-        // Set name on UI
-        FPS_GUI.Instance.weaponActive.text = weapons[currentWeaponIndex].name;
+        // Check if the index is out of bounds
+        if (currentWeaponIndex >= weapons.Count) currentWeaponIndex = 0;
+        if (currentWeaponIndex < 0) currentWeaponIndex = (byte)(weapons.Count - 1);
+
+        // Activate the new weapon
+        weapons[currentWeaponIndex].SetActive(true);
     }
 
+    #region Helper utilities
     // Add weapon
     public void AddWeapon(GameObject weapon) => weapons.Add(weapon); 
+
+    // Remove weapon
+    public void RemoveWeapon(GameObject weapon) => weapons.Remove(weapon);
+
+    // Get the current weapon
+    public GameObject GetCurrentWeapon() => weapons[currentWeaponIndex];
+
+    // Get the current weapon index
+    public byte GetCurrentWeaponIndex() => currentWeaponIndex;
+    #endregion
 }
