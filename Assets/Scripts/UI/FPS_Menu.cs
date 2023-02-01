1528aff902898f07a5bf728e026b5d10af7b4544
EAR19000614 - Joseph Early <EAR19000614@wakecoll.net>
2023-02-01 13:35:54 +0000

    Clean up

--- Assets/Scripts/UI/FPS_Menu.cs
+++ Assets/Scripts/UI/FPS_Menu.cs
@@ -3,6 +3,6 @@ using UnityEngine.SceneManagement;
 
 public class FPS_Menu : MonoBehaviour
 {
-    public static void StartGame() => SceneManager.LoadScene("Game");
+    public static void StartGame() => SceneManager.LoadScene("GameLevel");
     public static void QuitGame() => Application.Quit();
 }
